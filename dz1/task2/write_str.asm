3e2b429d8b98cbcc34771c3f90e86ecef6aa014d
Rikitick <rick.tick15@mail.ru>
2026-01-11 14:18:26 +0400

    Добавление проверки на количество выведенных байт

--- dz1/task2/write_str.asm
+++ dz1/task2/write_str.asm
@@ -5,6 +5,18 @@ section .data
     message db "Hello! How are you?", 10        ; строка для записи
     length equ $ - message                      ; длина строки
 
+    write_err db "Write failed!", 10
+    len_write_err equ $ - write_err
+
+    partial_err db "Partial write (ERROR)", 10
+    len_partial_err equ $ - partial_err
+
+    open_err db "Open failed!", 10
+    len_open_err equ $ - open_err
+
+    close_err db "Close failed!", 10
+    len_close_err equ $ - close_err
+
 section .text
 _start:
     mov rax, 2                              ; 2 - sys_open
@@ -28,21 +40,69 @@ _start:
     
     cmp rax, 0
     jl write_failed
+    jz write_failed
+    cmp rax, length
+    jl partial_write
     
     mov rax, 3                              ; 3 - sys_close
     mov rdi, r8                             ; файловый дескриптор
     syscall
+
+    cmp rax, 0                              ; проверка на закрытие файла
+    jl close_failed
     
     mov rax, 60                             ; sys_exit
     mov rdi, 0                              ; код возврата 0 - всё норм
     syscall
 
 open_failed:
+    mov rax, 1                              ; sys_write
+    mov rdi, 2                              ; stderr
+    mov rsi, open_err
+    mov rdx, len_open_err
+    syscall
+
     mov rax, 60
     mov rdi, 1                              ; код ошибки для open
     syscall
 
+close_failed:
+    mov rax, 1                              ; sys_write
+    mov rdi, 2                              ; stderr
+    mov rsi, close_err
+    mov rdx, len_close_err
+    syscall
+
+    mov rax, 60
+    mov rdi, 4                              ; код ошибки для close
+    syscall
+
 write_failed:
+    mov rax, 1                              ; sys_write
+    mov rdi, 2                              ; stderr
+    mov rsi, write_err
+    mov rdx, len_write_err
+    syscall
+
+    mov rax, 3                              ; sys_close
+    mov rdi, r8
+    syscall
+
     mov rax, 60
     mov rdi, 2                              ; код ошибки для write
+    syscall
+
+partial_write:
+    mov rax, 1                              ; sys_write
+    mov rdi, 2                              ; stderr
+    mov rsi, partial_err
+    mov rdx, len_partial_err
+    syscall
+    
+    mov rax, 3                              ; sys_close
+    mov rdi, r8
+    syscall
+    
+    mov rax, 60
+    mov rdi, 3                              ; код ошибки 3 - частичная запись
     syscall
